3a242d2997b0bb41a061c57b571b3ab37b2889c7
mateus jonatam <mateusgato1913@hotmail.com>
2019-06-14 22:04:53 -0300

    Arrumando bugs

--- app/services/comunicacao_wheater.rb
+++ app/services/comunicacao_wheater.rb
@@ -6,10 +6,5 @@ class ComunicacaoWheater
         url = "https://samples.openweathermap.org/data/2.5/weather?q=#{@cidade},uk&appid=03e0f7d40c9c2afd73ffd62cb2f82a34"
 
         retorno = JSON.parse(Net::HTTP.get(URI(url)))
-
-    rescue => exception
-        {erro: "Ligar no suporte"}
-    rescue JSON::ParseError => exception
-        {erro: "Ligar para o suporte!"}
     end
 end
