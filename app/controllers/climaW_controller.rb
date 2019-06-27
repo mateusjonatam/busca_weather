cc5243b1ab541f6ca57728ee0e71c73bbed7f957
mateus jonatam <mateusgato1913@hotmail.com>
2019-06-27 11:38:00 -0300

    Criacao de busca por cidade

--- app/controllers/climaW_controller.rb
+++ app/controllers/climaW_controller.rb
@@ -2,11 +2,9 @@ class BuscaClimaController < ApplicationController
     require 'net/http'
     require 'json'
 
+
     def buscar
         render json: ComunicacaoWeather.new.buscar(cidade_params[:cidade]), status: :ok
-
-    def cidade_params
-        params.permit(:cidade)
-    end
+    
 
 end
