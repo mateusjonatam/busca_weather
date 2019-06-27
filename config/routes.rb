1a360cf76f5c702f7904bf72d171e1614c3e67d7
mateus jonatam <mateusgato1913@hotmail.com>
2019-06-27 11:46:36 -0300

    Criacao da rota para buscar no site

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :cidades
   # For details on the DSL available within this file,ee http://guides.rubyonrails.org/routing.html
 
-
   get "busca_clima/buscar"
 
 
