d38371c3c31751f1f92cdc32b0861ce5b3ecbb63
mateus jonatam <mateusgato1913@hotmail.com>
2019-06-27 12:03:14 -0300

    Criacao de variavel para armazenar o hash da url

--- app/services/comunicacao_weather_hora.rb
+++ app/services/comunicacao_weather_hora.rb
@@ -5,7 +5,9 @@ class HourlyForecast
 
         key = "03e0f7d40c9c2afd73ffd62cb2f82a34"
 
-       
+        url = "http://api.openweathermap.org/data/2.5/forecast/hourly?q=#{@cidade},BR&appid=#{@key}&units=metric"
+    
+        retorno = JSON.parse(Net::HTTP.get(URI(url)))
 
     end
 end
