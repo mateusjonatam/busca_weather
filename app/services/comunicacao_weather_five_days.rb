1c4b05253b7e30c510b657a12dcaab0ad1b77fdd
mateus jonatam <mateusgato1913@hotmail.com>
2019-06-27 12:14:24 -0300

    Criacao de chamada de api atravez de variavel url

--- app/services/comunicacao_weather_five_days.rb
+++ app/services/comunicacao_weather_five_days.rb
@@ -4,5 +4,11 @@ class FiveDaysForecast
 
         @cidade = cidade
 
+
+        url = "http://api.openweathermap.org/data/2.5/forecast?q=#{@cidade},BR&appid=03e0f7d40c9c2afd73ffd62cb2f82a34&units=metric"
+    
+        retorno = JSON.parse(Net::HTTP.get(URI(url)))
+
+
     end
 end
