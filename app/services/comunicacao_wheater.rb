3fd54994c62420d3b6ebfc716b5f3608f127c822
mateus jonatam <mateusgato1913@hotmail.com>
2019-06-27 11:32:47 -0300

    Criacao de verificacao se existe cidade

--- app/services/comunicacao_wheater.rb
+++ app/services/comunicacao_wheater.rb
@@ -7,8 +7,12 @@ class ComunicacaoWheater
             url = "http://api.openweathermap.org/data/2.5/weather?q=#{@cidade},BR&appid=03e0f7d40c9c2afd73ffd62cb2f82a34&units=metric"
         
             retorno = JSON.parse(Net::HTTP.get(URI(url)))
-    
-                }
+
+
+            if retorno["message"]
+                return {message: "Cidade inexistente!"}
+            else
+               
             end
         end
     end
