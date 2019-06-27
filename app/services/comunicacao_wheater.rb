ede2b3ad12cc91abf4aa47047f3a10159d966297
mateus jonatam <mateusgato1913@hotmail.com>
2019-06-27 11:50:12 -0300

    Criacao do link para acessar o site da api

--- app/services/comunicacao_wheater.rb
+++ app/services/comunicacao_wheater.rb
@@ -3,7 +3,8 @@ class ComunicacaoWheater
         def atual(cidade)
 
             @cidade = cidade
-            
+            url = "http://api.openweathermap.org/data/2.5/weather?q=#{@cidade},BR&appid=03e0f7d40c9c2afd73ffd62cb2f82a34&units=metric"
+        
             retorno = JSON.parse(Net::HTTP.get(URI(url)))
 
 
